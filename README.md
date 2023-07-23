441bf30a1c2cf21961e16c6dcc05e1c937324865
jeanluchabi <jeanluchabi@gmail.com>
2023-07-23 10:47:06 -0700

    main.h

---
+++ README.md
@@ -0,0 +1,15 @@
+The group project on printf done by  NSHUTI Pacifique and Jean Luc HABIMANA as a team.
+
+Tasks
+0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life.
+Write a function that produces output according to a format.
+
+1. Education is when you read the fine print. Experience is what you get if you don't
+Handle the following conversion specifiers:
+
+d
+i
+You don’t have to handle the flag characters
+You don’t have to handle field width
+You don’t have to handle precision
+You don’t have to handle the length modifiers.
---
+++ main.h
@@ -0,0 +1,72 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+// FLAGS
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+// SIZES
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct - struct operation by Pacifique and Jean Luc
+ * @fmt: the format
+ * @af: associated function.
+ */
+struct fmt
+{
+	char fmt; 
+	int (*af)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct fmt fmt_r - structs opearation
+ *
+ * @fmt: format
+ * @fm_r: The associated function.
+ */
+typedef struct fmt fmt_r;
+
+int _printf(const char *format, ...);
+int handle_print(constant char *fmt, int *i, va_list list, char buffer[], int width, int precision, int size);
+
+/****************** FUNCTIONS ******************/
+
+/* printing chars and strings */
+int print_char(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+
+/* printing numbers */
+int print_int(va_list types, char buffer[],
+        int flags, int width, int precision, int size)
+int print_binary(va_list types, char buffer[],
+        int flags, int width, int precision, int size)
+int print_unsigned(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char bufffer[],
+	int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int print_hexa(va_list types, char map_to[],
+char buffer[], flags, char flag_ch, int width, int precision, int size);
+
+
