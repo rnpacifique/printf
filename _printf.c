d899c86c0557824fbbc86b9dd380a7bd3acf0eec
rnpacifique <rpacifique12@gmail.com>
2023-07-24 06:20:59 -0700

    formation of file arguments,c and _printf.c

--- _printf.c
+++ _printf.c
@@ -1,70 +1,29 @@
+#include <stdint.h>
 #include "main.h"
 
 /**
  * _printf - print formated output
- * @format: format string conatain characters
+ * @format: format string contain characters
  * Return: formated characters
  */
-
-
- int _printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
-	char buffer[BUFF_SIZE];
-	int flags;
-       	int width;
-       	int size;
-	int precision;
-	int buff_index = 0;
-	int a, printed = 0, all_chars = 0;
-	va_list arguments;
+	int printed_chars;
 
-	va_start(arguments, format)
+	conver_t func_list[] = {{"c", print_char}, {"s", print_string},
+		{"%", print_percent}, {"d", print_int}, {"i", print_int}, {NULL, NULL}};
+
+	va_list elements;
 
 	if (format == NULL)
 	{
 		return (-1);
 	}
-	for (a = 0; format && format[a] != '\0'; a++)
-	{
-		if (format(a) != '%')
-		{
-			buffer[buff_index++] = format[a];
-			if (buff_index == BUFF_SIZE)
-				print_buff(buffer, &buff_index);
-			all_chars++;
-		}
-		else
-		{
-			print_buff(buffer, &buff_index);
-			flags = get_flags(format, &a);
-			width = get_width(format, &a, arguments);
-			size = get_size(format, &a);
-			precision = get_precision(format, &a, arguments);
-			++a;
+	va_start(elements, format);
 
-			printed = handle_print(format, &a, arguments, 
-					buffer, flags, width, size, precision);
-			if (prited == -1)
-			{
-				return (-1);
-			}
-			all_chars = all_chars + printed;
-		}
-	}
+	num_printed = parser(format, func_list, elements);
 
-	print_buff(buffer, &buff_index);
-
-	va_end(list);
-	return(all_chars);
+	va_end(elements);
+	return (num_printed);
 }
-/**
- * print_buff - prints buffer if it is available
- * @buffer: sequence of chars
- * buff_index: Position for inserting the next character, indicating the length
- */
-viod print_buff(char buffer[], int *buff_index)
-{
-	if (*buff_index > 0)
-		write(1, &buffer[0], *buff_index);
-	*buff_index = 0;
-}
+
---
+++ arguments.c
@@ -0,0 +1,128 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers
+ * @elements: list of arguments
+ * Return: length of arguments
+ */
+int print_number(va_list elements)
+{
+	int nbr;
+	unsigned int n;
+	int leng;
+	int check;
+
+	nbr = va_arg(elements, int);
+	check = 1;
+	leng = 0;
+
+	if (nbr < 0)
+	{
+		leng = leng + _putchar('-');
+		n = nbr * -1;
+	}
+	else
+		n = nbr;
+	for (; n / check > 9;)
+		ckeck *= 10;
+	for (; check != 0;)
+	{
+		leng += _putchar('0' + n / check);
+		n % = check;
+		check /= 10;
+	}
+	return (leng);
+}
+/**
+ * print_unsigned_number - print an unsigned number
+ * @nbr: an unsigned number
+ * Return: Number of chars printed.
+*/
+int print_unsigned_number(unsigned int nbr)
+{
+	int leng;
+	unsigned int n;
+
+	leng = 0;
+	check = 1;
+
+	n = nbr;
+
+	for (; n / check > 9;)
+		check *= 10;
+	for (; check != 0;)
+	{
+		leng = leng = leng + _putchar('0' + n / check);
+		n %= check;
+		check /= 10;
+	}
+	return (leng);
+}
+
+/**
+ * print_char - print characters
+ * @elements: list of arguments
+ * Return: char
+ */
+
+int print_char(va_list elements)
+{
+	char c;
+
+	c = va_arg(elements, int);
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_int - print integer
+ * @elements: list of argments
+ * Return: int
+ */
+
+int print_int(va_list elements)
+{
+	int a;
+
+	a = print_number(elements);
+	return (a);
+}
+
+/**
+ * print_percent - print %
+ * @elements: list of argments
+ * Return: '%'
+ */
+
+int print_percent(_attribute_((unused)) va_list elements)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * print_string - print string
+ * @elements: list of argments
+ * Return: char
+ */
+
+int print_string(va_list elements)
+{
+	int a;
+	const char *str;
+
+	str = va_arg(elements, const char *);
+	if (str == NULL)
+	{
+		str = "null";
+	}
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	return (a);
+}
+
+
+
+
